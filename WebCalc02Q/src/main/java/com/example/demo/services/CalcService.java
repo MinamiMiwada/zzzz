1bb6974485e8c64f83cdf85b1146521a5ff3ead4
TJS0404\TJS-ADM-00404 <m-miwada@techno-jouhou.co.jp>
2022-11-15 11:44:16 +0900

    掛け算

--- WebCalc02Q/src/main/java/com/example/demo/services/CalcService.java
+++ WebCalc02Q/src/main/java/com/example/demo/services/CalcService.java
@@ -33,7 +33,17 @@ public class CalcService {
 		return AdResult;
 
 	}
-
+	
+	public String calculatemultiply( String NumA,String NumB) {
+		
+		
+		BigDecimal AdA = new BigDecimal(NumA);
+		BigDecimal AdB = new BigDecimal(NumB);
+		BigDecimal AdR = AdA.multiply(AdB);
+		String AdResult = AdR.toString();
+		
+		return AdResult;
+	}
 
 	//		割り算に関しては特殊なので注意
 	//		
